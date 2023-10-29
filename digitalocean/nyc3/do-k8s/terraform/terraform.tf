924c80d479473b31aa8897887ce42cfba25d7ab6
Vyas Nellutla <vyasnellutla@gmail.com>
2023-10-29 17:17:34 -0500

    fix: again

--- digitalocean/nyc3/do-k8s/terraform/terraform.tf
+++ digitalocean/nyc3/do-k8s/terraform/terraform.tf
@@ -29,16 +29,16 @@ terraform {
 # - https://registry.terraform.io/providers/digitalocean/digitalocean/latest/docs/resources/kubernetes_cluster#kubernetes-terraform-provider-example
 
 provider "kubernetes" {
-  host                   = data.tfe_outputs.digitalocean_nyc3_terraform.nonsensitive_values.host
-  token                  = data.tfe_outputs.digitalocean_nyc3_terraform.values.token
-  cluster_ca_certificate = data.tfe_outputs.digitalocean_nyc3_terraform.values.cluster_ca_certificate
+  host                   = data.tfe_outputs.digitalocean_nyc3_terraform.nonsensitive_values.do_k8s_host
+  token                  = data.tfe_outputs.digitalocean_nyc3_terraform.values.do_k8s_token
+  cluster_ca_certificate = data.tfe_outputs.digitalocean_nyc3_terraform.values.do_k8s_cluster_ca_certificate
 }
 
 provider "helm" {
   kubernetes {
-    host                   = data.tfe_outputs.digitalocean_nyc3_terraform.nonsensitive_values.host
-    token                  = data.tfe_outputs.digitalocean_nyc3_terraform.values.token
-    cluster_ca_certificate = data.tfe_outputs.digitalocean_nyc3_terraform.values.cluster_ca_certificate
+    host                   = data.tfe_outputs.digitalocean_nyc3_terraform.nonsensitive_values.do_k8s_host
+    token                  = data.tfe_outputs.digitalocean_nyc3_terraform.values.do_k8s_token
+    cluster_ca_certificate = data.tfe_outputs.digitalocean_nyc3_terraform.values.do_k8s_cluster_ca_certificate
   }
 }
 
