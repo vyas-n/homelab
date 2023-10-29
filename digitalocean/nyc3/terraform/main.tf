c181d664184c01877fd7af2b4b5afcff0f596690
Vyas Nellutla <vyasnellutla@gmail.com>
2023-10-29 16:21:16 -0500

    fix: data ref

--- digitalocean/nyc3/terraform/main.tf
+++ digitalocean/nyc3/terraform/main.tf
@@ -1,12 +1,8 @@
-data "digitalocean_kubernetes_versions" "do_k8s" {
-  # Grab the latest k8s version slug with `doctl kubernetes options versions`
-  version_prefix = "1.27."
-}
 
 resource "digitalocean_kubernetes_cluster" "do_k8s" {
   name          = "do-k8s"
   region        = var.region
-  version       = data.digitalocean_kubernetes_versions.example.latest_version
+  version       = data.digitalocean_kubernetes_versions.do_k8s.latest_version
   surge_upgrade = true
   auto_upgrade  = true
 
