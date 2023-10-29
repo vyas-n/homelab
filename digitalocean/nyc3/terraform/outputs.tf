58e322c80a4d954e2170ad57353a2b4ebc46d64e
Vyas Nellutla <vyasnellutla@gmail.com>
2023-10-29 16:18:36 -0500

    fix: cluster refs

--- digitalocean/nyc3/terraform/outputs.tf
+++ digitalocean/nyc3/terraform/outputs.tf
@@ -11,13 +11,13 @@ output "do_k8s_host" {
 output "do_k8s_token" {
   description = "The DigitalOcean API access token used by clients to access the cluster."
   sensitive   = true
-  value       = digitalocean_kubernetes_cluster.example.kube_config[0].token
+  value       = digitalocean_kubernetes_cluster.do_k8s.kube_config[0].token
 }
 
 output "do_k8s_cluster_ca_certificate" {
   description = "The public certificate for the cluster's certificate authority."
   sensitive   = false
   value = base64decode(
-    digitalocean_kubernetes_cluster.example.kube_config[0].cluster_ca_certificate
+    digitalocean_kubernetes_cluster.do_k8s.kube_config[0].cluster_ca_certificate
   )
 }
