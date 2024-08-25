549eedda6aea944dc26fb3f47b63b1fcdb345b51
Vyas <me@vyas-n.com>
2024-08-25 17:19:22 -0500

    feat: import resources (#296)
    * feat: import resources
    
    * fix: import tfcloud workspaces
    
    * fix: resync
    
    * fix: again
    
    * feat: add bedrock project

--- digitalocean/nyc3/terraform/main.tf
+++ digitalocean/nyc3/terraform/main.tf
@@ -3,17 +3,30 @@ locals {
   region = "nyc3"
 }
 
+resource "digitalocean_project" "bedrock" {
+  name = "bedrock"
+}
+
+resource "digitalocean_project_resources" "bedrock" {
+  project = digitalocean_project.bedrock.id
+  resources = [
+    digitalocean_kubernetes_cluster.do_k8s.id
+  ]
+}
+
 resource "digitalocean_kubernetes_cluster" "do_k8s" {
   name          = "do-k8s"
-  region        = "nyc3"
+  region        = local.region
   version       = data.digitalocean_kubernetes_versions.do_k8s.latest_version
   surge_upgrade = true
   auto_upgrade  = true
 
   node_pool {
     name = "default"
-    # Grab available droplet sizes with `doctl compute size list`
-    size       = "s-1vcpu-512mb-10gb"
+    # Grab available droplet sizes with `doctl kubernetes options sizes`
+    # Since this pool size can't be changed later, I'm only adding the smallest node type they have.
+    # Additional pools will actually be the size of the cluster.
+    size       = "s-1vcpu-2gb"
     node_count = 1
   }
 }
---
+++ tfcloud/terraform/main.tf
@@ -0,0 +1,60 @@
+
+resource "tfe_workspace" "tfcloud_terraform" {
+  name              = "tfcloud_terraform"
+  working_directory = "tfcloud/terraform"
+  project_id        = data.tfe_project.default.id
+
+  auto_apply            = true
+  queue_all_runs        = false
+  file_triggers_enabled = false
+
+  vcs_repo {
+    identifier                 = "vyas-n/deploy"
+    github_app_installation_id = local.tfc_github_app_install_id
+  }
+}
+
+resource "tfe_workspace" "digitalocean_terraform" {
+  name              = "digitalocean_terraform"
+  working_directory = "digitalocean/terraform"
+  project_id        = data.tfe_project.default.id
+
+  auto_apply            = true
+  queue_all_runs        = false
+  file_triggers_enabled = false
+
+  vcs_repo {
+    identifier                 = "vyas-n/deploy"
+    github_app_installation_id = local.tfc_github_app_install_id
+  }
+}
+
+resource "tfe_workspace" "digitalocean_nyc3_terraform" {
+  name              = "digitalocean_nyc3_terraform"
+  working_directory = "digitalocean/nyc3/terraform"
+  project_id        = data.tfe_project.default.id
+
+  auto_apply            = true
+  queue_all_runs        = false
+  file_triggers_enabled = false
+
+  vcs_repo {
+    identifier                 = "vyas-n/deploy"
+    github_app_installation_id = local.tfc_github_app_install_id
+  }
+}
+
+resource "tfe_workspace" "digitalocean_nyc3_do-k8s_terraform" {
+  name              = "digitalocean_nyc3_do-k8s_terraform"
+  working_directory = "digitalocean/nyc3/do-k8s/terraform"
+  project_id        = data.tfe_project.default.id
+
+  auto_apply            = true
+  queue_all_runs        = false
+  file_triggers_enabled = false
+
+  vcs_repo {
+    identifier                 = "vyas-n/deploy"
+    github_app_installation_id = local.tfc_github_app_install_id
+  }
+}
