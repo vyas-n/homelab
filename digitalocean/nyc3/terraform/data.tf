9559adc47a2cfbe2c3f6ce11a73a9455bf5d16c6
Vyas <me@vyas-n.com>
2024-09-17 21:24:39 -0500

    chore: bump k8s (#300)

--- digitalocean/nyc3/terraform/data.tf
+++ digitalocean/nyc3/terraform/data.tf
@@ -1,5 +1,5 @@
 
 data "digitalocean_kubernetes_versions" "do_k8s" {
   # Grab the latest k8s version slug with `doctl kubernetes options versions`
-  version_prefix = "1.30."
+  version_prefix = "1.31."
 }
