3382fb5dc6727b9561fac756cafb50f9462bf933
Vyas <me@vyas-n.com>
2024-11-24 13:28:29 -0500

    feat: add 1pass connect (#322)
    * feat: add 1pass connect
    
    * refactor: use 1pass tf provider
    
    * refactor: replace bash variables
    
    * fix: init 1pass connect secrets
    
    * fix: typo
    
    * feat: add tfc workspace
    
    * fix: again
    
    * fix: retry tfc workflow
    
    * fix: retry tfc workflow
    
    * fix: retry tfc workflow
    
    * fix: retry tfc workflow
    
    * feat: add namespaces
    
    * fix: the things
    
    * fix: add values
    
    * fix: retry tfc workflow
    
    * fix: retry tfc workflow

--- digitalocean/nyc3/do-k8s/terraform/main.tf
+++ digitalocean/nyc3/do-k8s/terraform/main.tf
@@ -24,3 +24,50 @@ resource "helm_release" "authentik" { # ref: https://artifacthub.io/packages/hel
     yamlencode({})
   ]
 }
+
+resource "kubernetes_namespace" "onepassconnect" {
+  metadata {
+    name = "onepassconnect"
+  }
+}
+
+resource "kubernetes_secret" "onepass_connect_credentials_json" {
+  metadata {
+    name      = "op-credentials"
+    namespace = kubernetes_namespace.onepassconnect.metadata[0].name
+  }
+  data = {
+    "1password-credentials.json" = base64encode(var.onepassword_credentials_json)
+  }
+}
+
+resource "helm_release" "onepassconnect" { # ref: https://artifacthub.io/packages/helm/onepassword-connect/connect
+  name       = "connect"
+  chart      = "connect"
+  repository = "https://1password.github.io/connect-helm-charts"
+  version    = "1.16.0"
+
+  max_history      = 5
+  namespace        = kubernetes_namespace.onepassconnect.metadata[0].name
+  create_namespace = false
+  lint             = true
+  timeout          = 600
+  wait             = true
+  wait_for_jobs    = true
+
+  values = [
+    yamlencode(yamldecode(file("${path.module}/helm/onepassconnect/values.yaml"))), # remove yaml comments & formatting from diff calculations
+    yamlencode({
+      connect = {
+        # The name of Kubernetes Secret containing the 1Password Connect credentials
+        credentialsName = kubernetes_secret.onepass_connect_credentials_json.metadata[0].name
+      }
+    })
+  ]
+}
+
+resource "kubernetes_namespace" "external_secrets" {
+  metadata {
+    name = "external-secrets"
+  }
+}
---
+++ secrets/terraform/data.tf
@@ -0,0 +1,42 @@
+
+data "onepassword_vault" "private" {
+  name = "Private"
+}
+
+# Terraform Cloud
+data "onepassword_item" "tfcloud_pat" {
+  vault = data.onepassword_vault.private.uuid
+  title = "TerraformCloud-PAT"
+}
+data "onepassword_item" "tfcloud_org_token_vyasn" {
+  vault = data.onepassword_vault.private.uuid
+  title = "TFC Org Token - vyas-n"
+}
+
+# Cloudflare
+data "onepassword_item" "cloudflare_pat" {
+  vault = data.onepassword_vault.private.uuid
+  title = "Cloudflare-PAT"
+}
+
+# DigitalOcean
+data "onepassword_item" "digitalocean_pat_for_tfcloud_vyasn" {
+  vault = data.onepassword_vault.private.uuid
+  title = "DigitalOcean-PAT-for-Terraform-Cloud-vyas-n"
+}
+data "onepassword_item" "digitalocean_s3access_for_tfcloud_vyasn" {
+  vault = data.onepassword_vault.private.uuid
+  title = "DigitalOcean-S3Access-for-Terraform-Cloud-vyas-n"
+}
+
+# Bedrock 1PassConnect Server
+data "onepassword_item" "onepass_connect_credentials_json" {
+  vault = data.onepassword_vault.private.uuid
+  title = "1Pass Connect Server: Bedrock"
+}
+
+# Bedrock 1PassConnect Access Token
+data "onepassword_item" "onepass_connect_access_token" {
+  vault = data.onepassword_vault.private.uuid
+  title = "1Pass Connect Bedrock Access Token: Kubernetes"
+}
