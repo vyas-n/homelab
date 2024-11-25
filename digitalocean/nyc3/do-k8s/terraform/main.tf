1b7d1249bdb94496ea6d6100ef17fae1c057acec
Vyas <me@vyas-n.com>
2024-11-25 09:29:11 -0500

    feat: deploy external-secrets (#328)
    * feat: deploy external-secrets
    
    * fix: again
    
    * fix: try again
    
    * fix: try again
    
    * fix: provider
    
    * fix: again

--- digitalocean/nyc3/do-k8s/terraform/main.tf
+++ digitalocean/nyc3/do-k8s/terraform/main.tf
@@ -5,7 +5,8 @@ resource "kubernetes_namespace" "authentik" {
   }
 }
 
-resource "helm_release" "authentik" { # ref: https://artifacthub.io/packages/helm/goauthentik/authentik
+# ref: https://artifacthub.io/packages/helm/goauthentik/authentik
+resource "helm_release" "authentik" {
   name       = "authentik"
   chart      = "authentik"
   repository = "oci://ghcr.io/goauthentik/helm-charts"
@@ -20,7 +21,8 @@ resource "helm_release" "authentik" { # ref: https://artifacthub.io/packages/hel
   wait_for_jobs    = true
 
   values = [
-    yamlencode(yamldecode(file("${path.module}/helm/authentik/values.yaml"))), # remove yaml comments & formatting from diff calculations
+    # remove yaml comments & formatting from diff calculations
+    yamlencode(yamldecode(file("${path.module}/helm/authentik/values.yaml"))),
     yamlencode({})
   ]
 }
@@ -41,7 +43,8 @@ resource "kubernetes_secret" "onepass_connect_credentials_json" {
   }
 }
 
-resource "helm_release" "onepassconnect" { # ref: https://artifacthub.io/packages/helm/onepassword-connect/connect
+# ref: https://artifacthub.io/packages/helm/onepassword-connect/connect
+resource "helm_release" "onepassconnect" {
   name       = "connect"
   chart      = "connect"
   repository = "https://1password.github.io/connect-helm-charts"
@@ -56,7 +59,8 @@ resource "helm_release" "onepassconnect" { # ref: https://artifacthub.io/package
   wait_for_jobs    = true
 
   values = [
-    yamlencode(yamldecode(file("${path.module}/helm/onepassconnect/values.yaml"))), # remove yaml comments & formatting from diff calculations
+    # remove yaml comments & formatting from diff calculations
+    yamlencode(yamldecode(file("${path.module}/helm/onepassconnect/values.yaml"))),
     yamlencode({
       connect = {
         # The name of Kubernetes Secret containing the 1Password Connect credentials
@@ -70,4 +74,102 @@ resource "kubernetes_namespace" "external_secrets" {
   metadata {
     name = "external-secrets"
   }
+}
+
+# ref: https://artifacthub.io/packages/helm/external-secrets-operator/external-secrets
+resource "helm_release" "external_secrets" {
+  name       = "external-secrets"
+  chart      = "external-secrets"
+  repository = "oci://ghcr.io/external-secrets/charts"
+  version    = "0.10.7"
+
+  max_history      = 5
+  namespace        = kubernetes_namespace.external_secrets.metadata[0].name
+  create_namespace = false
+  lint             = true
+  timeout          = 600
+  wait             = true
+  wait_for_jobs    = true
+
+  values = [
+    # remove yaml comments & formatting from diff calculations
+    yamlencode(yamldecode(file("${path.module}/helm/external-secrets/values.yaml"))),
+    yamlencode({})
+  ]
+}
+
+resource "kubernetes_secret" "onepassword_connect_access_token" {
+  metadata {
+    name      = "onepassword-connect-token"
+    namespace = kubernetes_namespace.external_secrets.metadata[0].name
+  }
+  data = {
+    token = var.onepassword_access_token
+  }
+}
+
+resource "kubectl_manifest" "onepass_cluster_secret_store" {
+  yaml_body = yamlencode({
+    apiVersion = "external-secrets.io/v1beta1"
+    kind       = "ClusterSecretStore"
+    metadata = {
+      name = "onepass"
+    }
+    spec = {
+      provider = {
+        onepassword = {
+          connectHost = "http://onepassword-connect.onepassconnect.svc:8080"
+          vaults = {
+            # look in this vault first & only
+            Bedrock = 1
+          }
+          auth = {
+            secretRef = {
+              connectTokenSecretRef = {
+                name = kubernetes_secret.onepassword_connect_access_token.metadata[0].name
+                key  = "token"
+              }
+            }
+          }
+        }
+      }
+    }
+  })
+}
+
+
+resource "kubectl_manifest" "authentik_eso_secrets" {
+  yaml_body = yamlencode({
+    apiVersion = "external-secrets.io/v1beta1"
+    kind       = "ExternalSecret"
+    metadata = {
+      name      = "authentik-eso-secrets"
+      namespace = kubernetes_namespace.authentik.metadata[0].name
+    }
+    spec = {
+      secretStoreRef = {
+        kind = "ClusterSecretStore"
+        name = kubectl_manifest.onepass_cluster_secret_store.name
+      }
+      target = {
+        creationPolicy = "Owner"
+      }
+      data = [
+        {
+          secretKey = "pg_pass"
+          remoteRef = {
+            key      = "Authentik"
+            property = "postgres password"
+          }
+        },
+        {
+          secretKey = "django_secret_key"
+          remoteRef = {
+            key      = "Authentik"
+            property = "django secret key"
+          }
+        }
+      ]
+    }
+  })
 }
