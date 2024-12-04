0e20517202f564271329e0af4f527d9cc456ca58
Vyas <me@vyas-n.com>
2024-12-04 09:09:31 -0600

    fix: k8s creds (#333)

--- digitalocean/nyc3/do-k8s/terraform/terraform.tf
+++ digitalocean/nyc3/do-k8s/terraform/terraform.tf
@@ -34,6 +34,12 @@ provider "kubernetes" {
   cluster_ca_certificate = data.tfe_outputs.digitalocean_nyc3_terraform.values.do_k8s_cluster_ca_certificate
 }
 
+provider "kubectl" {
+  host                   = data.tfe_outputs.digitalocean_nyc3_terraform.nonsensitive_values.do_k8s_host
+  token                  = data.tfe_outputs.digitalocean_nyc3_terraform.values.do_k8s_token
+  cluster_ca_certificate = data.tfe_outputs.digitalocean_nyc3_terraform.values.do_k8s_cluster_ca_certificate
+}
+
 provider "helm" {
   kubernetes {
     host                   = data.tfe_outputs.digitalocean_nyc3_terraform.nonsensitive_values.do_k8s_host
