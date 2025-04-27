5cdff292603b8425f350ef06e0c2d9816c8cd145
Vyas Nellutla <me@vyas-n.com>
2025-04-26 23:21:34 -0500

    feat: configure tailscale
    Signed-off-by: Vyas Nellutla <me@vyas-n.com>

---
+++ tailscale/terraform/terraform.tf
@@ -0,0 +1,22 @@
+/*
+ * # Tailscale Global Config Root
+ *
+*/
+
+terraform {
+  required_version = ">= 1.11.0"
+  cloud {
+    organization = "vyas-n"
+    workspaces {
+      name = "tailscale_terraform"
+    }
+  }
+  required_providers {
+    tailscale = {
+      source  = "tailscale/tailscale"
+      version = "0.19.0"
+    }
+  }
+}
+
+provider "tailscale" {}
