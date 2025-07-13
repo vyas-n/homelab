19c4209b7c793b7a395d8b94b496be59039315f0
Vyas Nellutla <me@vyas-n.com>
2025-07-12 21:38:43 -0500

    refactor: a bunch of stuff

--- k8s/digitalocean/terraform/terraform.tf
+++ k8s/digitalocean/terraform/terraform.tf
@@ -8,7 +8,7 @@ terraform {
   cloud {
     organization = "vyas-n"
     workspaces {
-      name = "digitalocean_nyc3_do_k8s_terraform"
+      name = "k8s_digitalocean_terraform"
     }
   }
   required_providers {
