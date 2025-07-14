6fb7e3c252e9483549aae0fb1afdf9317c946215
Vyas Nellutla <me@vyas-n.com>
2025-07-13 20:36:20 -0500

    docs: adjust flake description
    Signed-off-by: Vyas Nellutla <me@vyas-n.com>

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "My personal website used to learn and test things.";
+  description = "My homelab!";
 
   inputs = {
     nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-25.05";
