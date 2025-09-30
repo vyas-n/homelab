49d59b6974253643da3cd8f497cafc039b67dc5f
Vyas Nellutla <me@vyas-n.com>
2025-09-30 11:47:50 -0500

    fix: try fixing external-dns

--- infra/k8s/homezone/terraform/modules/cluster_operators/main.tf
+++ infra/k8s/homezone/terraform/modules/cluster_operators/main.tf
@@ -226,9 +226,6 @@ resource "helm_release" "external_dns_cloudflare" { # https://artifacthub.io/pac
           }
         }
       }]
-      domainFilters = [
-        "svc.vyas-n.dev"
-      ]
     })
   ]
 }
