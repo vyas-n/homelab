d6ce584baef5ec96cd9d9f79554609a58663183c
Vyas Nellutla <me@vyas-n.com>
2025-12-14 23:03:01 -0600

    feat: add k8s node

--- infra/proxmox/terraform/data.tf
+++ infra/proxmox/terraform/data.tf
@@ -1,4 +1,8 @@
 # Homelab Proxmox
+data "proxmox_virtual_environment_node" "proxmox_0" {
+  node_name = "proxmox-0"
+}
+
 data "proxmox_virtual_environment_node" "proxmox_1" {
   node_name = "proxmox-1"
 }
