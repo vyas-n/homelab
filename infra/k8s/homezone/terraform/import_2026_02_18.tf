526edbd705121bf871b480b41e456aee0ec3244f
Vyas Nellutla <me@vyas-n.com>
2026-02-22 06:01:45 -0600

    feat: bump cilium

--- infra/k8s/homezone/terraform/import_2026_02_18.tf
+++
@@ -1,9 +0,0 @@
-import {
-  id = "cert-manager/cert-manager"
-  to = module.cluster_operators.helm_release.cert_manager
-}
-
-import {
-  id = "kube-prometheus-stack/kube-prometheus-stack"
-  to = omodule.monitoring_stack.helm_release.kube_prometheus_stack
-}
--- infra/k8s/homezone/terraform/modules/cilium/main.tf
+++ infra/k8s/homezone/terraform/modules/cilium/main.tf
@@ -3,7 +3,7 @@ resource "helm_release" "cilium" { # https://artifacthub.io/packages/helm/cilium
   name       = "cilium"
   chart      = "cilium"
   repository = "https://helm.cilium.io"
-  version    = "1.17.6"
+  version    = "1.17.13"
 
   namespace        = "kube-system"
   create_namespace = false
