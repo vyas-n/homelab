a70c74aa6ff8a17d699aac01f2dfd08828dbfe08
Vyas Nellutla <me@vyas-n.com>
2026-03-12 21:42:45 -0500

    fix: use undocumented import statement

---
+++ infra/unifi/terraform/imports_2026_03_12.tf
@@ -0,0 +1,4 @@
+import {
+  to = unifi_client.truenas
+  id = "b0:19:21:e3:c5:f0"
+}
--- infra/unifi/terraform/main.tf
+++ infra/unifi/terraform/main.tf
@@ -9,6 +9,16 @@ resource "unifi_client" "tplink_managed_switch_1" {
 
 # TrueNAS
 
+resource "unifi_client" "truenas" {
+  mac  = "b0:19:21:e3:c5:f0"
+  name = "TrueNAS"
+
+  fixed_ip         = "192.168.2.244"
+  local_dns_record = "truenas.hosts.vyas-n.dev"
+}
+
+# Proxmox
+
 resource "unifi_client" "proxmox_1_ipmi" {
   mac  = "9c:6b:00:13:4a:a7"
   name = "Proxmox 1 (IPMI)"
@@ -17,7 +27,6 @@ resource "unifi_client" "proxmox_1_ipmi" {
   local_dns_record = "proxmox-1-ipmi.hosts.vyas-n.dev"
 }
 
-# Proxmox
 resource "unifi_client" "proxmox_1" {
   mac  = "9c:6b:00:13:4a:12"
   name = "Proxmox 1"
