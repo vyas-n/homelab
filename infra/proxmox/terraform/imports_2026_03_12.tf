089db9f53286afec51cbc9afb011dcff32eaf723
Vyas Nellutla <me@vyas-n.com>
2026-03-12 23:37:16 -0500

    fix: try again

--- infra/proxmox/terraform/imports_2026_03_12.tf
+++ infra/proxmox/terraform/imports_2026_03_12.tf
@@ -4,9 +4,9 @@ import {
 }
 import {
   to = module.k8s_ctr[1].module.dns.unifi_client.client_device
-  id = "BC:24:11:AF:F7:AF"
+  id = "bc:24:11:af:f7:af"
 }
 import {
   to = module.k8s_ctr[2].module.dns.unifi_client.client_device
-  id = "BC:24:11:58:93:3F"
+  id = "bc:24:11:58:93:3f"
 }
