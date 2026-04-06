e736726525f24f5c8f2326aadc3f5b3d8dddfeda
Vyas Nellutla <me@vyas-n.com>
2026-04-05 20:25:12 -0500

    feat: argo-cd app management

--- infra/k8s/homezone/terraform/terraform.tf
+++ infra/k8s/homezone/terraform/terraform.tf
@@ -65,7 +65,7 @@ provider "helm" {
 provider "time" {}
 
 provider "argocd" {
-  kubernetes = {
+  kubernetes {
     host                   = var.kube_host
     cluster_ca_certificate = var.kube_cluster_ca_cert_data
     client_certificate     = var.kube_client_cert_data
