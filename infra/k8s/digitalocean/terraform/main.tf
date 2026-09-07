abe31244a65c6f69574554516a25d9aa924b2596
Vyas Nellutla <me@vyas-n.com>
2026-09-07 10:50:52 -0500

    fix: more prek

--- infra/k8s/digitalocean/terraform/main.tf
+++ infra/k8s/digitalocean/terraform/main.tf
@@ -4,3 +4,13 @@ resource "kubernetes_namespace" "tailscale" {
     name = "tailscale"
   }
 }
+
+resource "tailscale_oauth_client" "k8s_tailscale_operator" {
+  description = "digitalocean-k8s-tailscale-operator"
+  scopes      = [
+    "services",
+    "devices:core",
+    "auth_keys",
+  ]
+  tags        = []
+}
