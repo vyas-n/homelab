0db653cb9a7c6259803d647d33ff843856903a96
Vyas Nellutla <me@vyas-n.com>
2026-09-07 21:02:21 -0500

    feat: setup tailscale-operator

--- infra/k8s/digitalocean/terraform/main.tf
+++ infra/k8s/digitalocean/terraform/main.tf
@@ -28,8 +28,8 @@ resource "kubernetes_secret_v1" "operator_oauth" {
 }
 
 resource "helm_release" "tailscale_operator" {
-  chart      = "tailscale/tailscale-operator"
-  repository = "https://pkgs.tailscale.com/helmcharts"
+  chart      = "tailscale-operator"
+  repository = "https://pkgs.tailscale.com/helmcharts/tailscale"
   name       = "tailscale-operator"
   namespace  = kubernetes_namespace_v1.tailscale.id
   wait       = true
