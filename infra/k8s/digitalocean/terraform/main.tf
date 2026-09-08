17ea154aa97bb10b0611a90abcd80b0b69078d99
Vyas Nellutla <me@vyas-n.com>
2026-09-07 21:00:38 -0500

    feat: setup tailscale-operator

--- infra/k8s/digitalocean/terraform/main.tf
+++ infra/k8s/digitalocean/terraform/main.tf
@@ -1,16 +1,41 @@
 
-resource "kubernetes_namespace" "tailscale" {
+resource "kubernetes_namespace_v1" "tailscale" {
   metadata {
     name = "tailscale"
   }
 }
 
+# https://tailscale.com/docs/kubernetes-operator/quickstart#prerequisites
 resource "tailscale_oauth_client" "k8s_tailscale_operator" {
   description = "digitalocean-k8s-tailscale-operator"
-  scopes      = [
+  scopes = [
     "services",
     "devices:core",
     "auth_keys",
   ]
-  tags        = ["tag:k8s-operator"]
+  tags = ["tag:k8s-operator"]
+}
+
+resource "kubernetes_secret_v1" "operator_oauth" {
+  metadata {
+    name      = "operator-oauth"
+    namespace = kubernetes_namespace_v1.tailscale.id
+  }
+  data = {
+    client_id     = tailscale_oauth_client.k8s_tailscale_operator.id
+    client_secret = tailscale_oauth_client.k8s_tailscale_operator.key
+  }
+}
+
+resource "helm_release" "tailscale_operator" {
+  chart      = "tailscale/tailscale-operator"
+  repository = "https://pkgs.tailscale.com/helmcharts"
+  name       = "tailscale-operator"
+  namespace  = kubernetes_namespace_v1.tailscale.id
+  wait       = true
+
+  values = [yamlencode({
+  })]
+
+  depends_on = [kubernetes_secret_v1.operator_oauth]
 }
