8e42d278607c199ed716112cfc4c0a4cef14d81d
Vyas Nellutla <me@vyas-n.com>
2026-09-16 11:45:37 -0500

    fix: kargo

--- infra/k8s/digitalocean/terraform/modules/kargo/versions.tf
+++ infra/k8s/digitalocean/terraform/modules/kargo/versions.tf
@@ -10,6 +10,10 @@ terraform {
       source  = "hashicorp/helm"
       version = ">= 3.0.2"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 3.2.1"
+    }
     random = {
       source  = "hashicorp/random"
       version = ">= 3.9.1"
